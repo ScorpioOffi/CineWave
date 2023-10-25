24e125f441a0afa3adae317280d6bbb9d22ddb79
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-10-25 13:41:10 +0200

    réglage branch this home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './css/App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
