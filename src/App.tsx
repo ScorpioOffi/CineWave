bf5cfc386123fbc88d5c6e6883467fd5589e0c27
Adilina04 <113524418+Adilina04@users.noreply.github.com>
2023-10-25 23:31:38 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import Login from './components/Login';
 import Home from './components/Home';
 import SeriesDetails from './components/SeriesDetails';
 import SignUp from './components/Register';
+import Header from './components/Header'
 
 function App() {
   return (
+<div>
+
     <BrowserRouter>
       <Routes>
-        <Route path="/accueil" element={<Home />} />
-        <Route path="/accueil/series/:id" Component={SeriesDetails} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/series/:id" Component={SeriesDetails} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
+    </div>
+
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { NavLink, useNavigate } from 'react-router-dom'
+import './../css/Header.css';
+
+const Header = () => {
+
+  return (
+    <header>
+
+      <nav>
+        <div className="links">
+
+          <NavLink to="/home">Calendar</NavLink>  
+          <NavLink to="/login">Shows</NavLink>
+          <NavLink to="/profile">Profil</NavLink>
+
+        </div>
+
+        <NavLink to="/profile">
+        <img className="icon" src="./assets/icon.png" alt="image" />  
+
+        </NavLink>
+
+      </nav>
+
+    </header>
+  )
+
+}
+
+export default Header;
