37c6ec5c728eec51e17bfbbc617101b5faf61648
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-10-26 15:14:28 +0200

    modif register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,85 +1,83 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import { AuthError, createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
 import { auth } from '../database/firebase.config';
- 
+
 const Signup = () => {
     const navigate = useNavigate();
- 
-    const [email, setEmail] = useState('')
+
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
- 
+
     const onSubmit = async (e: { preventDefault: () => void; }) => {
-      e.preventDefault()
-     
-      await createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
+        e.preventDefault();
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             const user = userCredential.user;
-            console.log(user);
-            navigate("/login")
-        })
-        .catch((error) => {
+
+            await sendEmailVerification(user);
+            console.log("Compte créé avec succès. Veuillez vérifier votre e-mail.");
+            navigate("/login");
+        } catch (error: any) {
             const errorCode = error.code;
             const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
-        });
- 
-   
-    }
- 
-  return (
-    <main >        
-        <section>
-            <div>
-                <div>                  
-                    <h1> Register </h1>                                                                            
-                    <form>                                                                                            
-                        <div>
-                            <label htmlFor="email-address">
-                                Email address
-                            </label>
-                            <input
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}  
-                                required                                    
-                                placeholder="Email address"                                
-                            />
-                        </div>
+            console.error("Erreur lors de la création du compte : ", errorCode, errorMessage);
+        }
+    };
 
-                        <div>
-                            <label htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)} 
-                                required                                 
-                                placeholder="Password"              
-                            />
-                        </div>                                             
-                        
-                        <button
-                            type="submit" 
-                            onClick={onSubmit}                        
-                        >  
-                            Sign up                                
-                        </button>
-                                                                     
-                    </form>
-                   
-                    <p>
-                        Already have an account?{' '}
-                        <NavLink to="/login" >
-                            Sign in
-                        </NavLink>
-                    </p>                   
+    return (
+        <main>
+            <section>
+                <div>
+                    <div>
+                        <h1> Register </h1>
+                        <form>
+                            <div>
+                                <label htmlFor="email-address">
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                    placeholder="Email address"
+                                />
+                            </div>
+
+                            <div>
+                                <label htmlFor="password">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                    placeholder="Password"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                onClick={onSubmit}
+                            >
+                                Sign up
+                            </button>
+                        </form>
+
+                        <p>
+                            Already have an account?{' '}
+                            <NavLink to="/login">
+                                Sign in
+                            </NavLink>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </section>
-    </main>
-  )
+            </section>
+        </main>
+    );
 }
- 
-export default Signup
+
+export default Signup;
